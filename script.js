fc3cfe102943bfc108723f67e7f4c485eef0bcd0
Himanshu Yadav <himanshu.tj@gmail.com>
2024-10-07 01:47:27 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var a=prompt("Enter UserName");
  var b=prompt("Enter password");
   	if(a== "Poonam" && b=="07-10-1992")
   	{
+		alert("Poonam");
   	    window.location.href="start.html";
   	}
   	else
